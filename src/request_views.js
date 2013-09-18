3066bad1d46fc9a5a02e6644a96954d9f3e7ecdf
David Emory <demory@conveyal.com>
2013-09-18 12:56:17 -0400

    only show mode-relevant input controls

--- src/request_views.js
+++ src/request_views.js
@@ -6,49 +6,57 @@ var Handlebars = require('handlebars');
 var requestFormTemplate = Handlebars.compile([
     
         '<div class="row-fluid" style="margin: 5px 0px;">',
-            '<select id="fromPlace" style="width: 100%" placeholder="Start Address"></select>',
+            '<select id="fromPlace" class="apiParam" style="width: 100%" placeholder="Start Address"></select>',
         '</div>',
         '<div class="row-fluid" style="margin: 5px 0px;">',
-            '<select id="toPlace" style="width: 100%"  placeholder="End Address"></select>',
+            '<select id="toPlace" class="apiParam" style="width: 100%"  placeholder="End Address"></select>',
         '</div>',
         '<div class="row-fluid">',
-            '<select id="arriveBy" class="span3" placeholder="Arrive"><option value="true">Arrive</option><option value="false" selected>Depart</option></select>',
+            '<select id="arriveBy" class="apiParam span3" placeholder="Arrive"><option value="true">Arrive</option><option value="false" selected>Depart</option></select>',
         
             '<div class="input-append bootstrap-timepicker">',
-                '<input id="time" type="text" class="input-small">',
+                '<input id="time" type="text" class="apiParam input-small">',
                 '<span class="add-on"><i class="icon-time"></i></span>',
             '</div>',
 
             '<div class="input-append date" data-date-format="mm/dd/yyyy">',
-                '<input id="date" class="input-small" size="16" type="text"><span class="add-on"><i class="icon-th"></i></span>',
+                '<input id="date" class="apiParam input-small" size="16" type="text"><span class="add-on"><i class="icon-th"></i></span>',
             '</div>',
     
             '<form class="form-horizontal">',
                 '<div class="control-group">',
                     '<label class="control-label" for="mode">Travel by: </label>',
                     '<div class="controls">',
-                        '<select id="mode" class="span12" placeholder="Arrive"><option value="TRANSIT,WALK">Transit</option><option value="WALK">Walk only</option><option value="BICYCLE">Bike only</option><option value="TRANSIT,BICYCLE">Transit & Bike</option></select>',
+                        '<select id="mode" class="apiParam span12" placeholder="Arrive"><option value="TRANSIT,WALK">Transit</option><option value="WALK">Walk only</option><option value="BICYCLE">Bike only</option><option value="TRANSIT,BICYCLE">Transit & Bike</option></select>',
                     '</div>',
                 '</div>',
 
-                '<div class="control-group">',
+                '<div class="optimizeControl control-group">',
                     '<label class="control-label" for="type">Find: </label>',
                     '<div class="controls">',
                         '<select id="optimize" class="span12" placeholder="Arrive"><option value="QUICK">Quickest trip</option><option value="TRANSFERS">Fewest transfers</option><option>Custom trip</option></select>',
                     '</div>',
                 '</div>',
 
-                '<div class="control-group">',
+                '<div class="bikeTriangleControl control-group">',
                     '<div id="bikeTriangle" style="height: 100px; cursor: pointer;">',
                     '</div>',    
                 '</div>',
 
-                '<div class="control-group">',
+                '<div class="maxWalkControl control-group">',
                     '<label class="control-label" for="maxWalkDist">Maximum walk: </label>',
                     '<div class="controls">',
-                        '<select id="maxWalkDistance" class="span12" placeholder="Arrive"><option value="402">1/4 mile</option><option value="804">1/2 mile</option><option value="1223" selected>3/4 mile</option><option value="1609">1 mile</option><option value="3218">2 miles</option><option value="4828">3 miles</option></select>',
+                        '<select id="maxWalkDistance" class="apiParam span12" placeholder="Arrive"><option value="402">1/4 mile</option><option value="804">1/2 mile</option><option value="1223" selected>3/4 mile</option><option value="1609">1 mile</option><option value="3218">2 miles</option><option value="4828">3 miles</option></select>',
                     '</div>',
                 '</div>',
+
+                '<div class="maxBikeControl control-group">',
+                    '<label class="control-label" for="maxWalkDist">Maximum bike: </label>',
+                    '<div class="controls">',
+                        '<select id="maxBikeDistance" class="apiParam span12" placeholder="Arrive"><option value="804">1/2 mile</option><option value="1609">1 mile</option><option value="4026">2.5 miles</option><option value="8047">5 miles</option><option value="16093">10 miles</option></select>',
+                    '</div>',
+                '</div>',
+
             '</form>',
         '</div>'
 ].join('\n'));
@@ -64,14 +72,8 @@ var geocodeItem = Handlebars.compile([
 var OtpRequestFormView = Backbone.View.extend({
  
     events: {
-        "change #fromPlace"     : "changeForm",
-        "change #toPlace"       : "changeForm",
-        "change #arriveBy"        : "changeForm",
-        "change #time"          : "changeForm",
-        "change #date"          : "changeForm",
-        "change #mode"          : "changeForm",
-        "change #optimize"          : "changeForm",
-        "change #maxWalkDistance" : "changeForm"
+        "change .apiParam"      : "changeForm",
+        "change #mode"          : "updateModeControls"
     },
 
     initialize : function() {
@@ -245,6 +247,8 @@ var OtpRequestFormView = Backbone.View.extend({
         view.updatingForm = false;
 
         this.changeForm();
+
+        this.updateModeControls();
     },
 
     changeForm: function(evt) {
@@ -253,13 +257,16 @@ var OtpRequestFormView = Backbone.View.extend({
         if(this.updatingForm)
             return;
 
+        var maxDistance = $('#mode').val().indexOf("WALK") != -1 ?
+            $('#maxWalkDistance').val() : $('#maxBikeDistance').val()
+
         var data = {
             fromPlace: $('#fromPlace').val(),
             toPlace: $('#toPlace').val(),
             date: $('#date').val(),
             time: $('#time').val(),
             arriveBy: $('#arriveBy').val(),
-            maxWalkDistance: $('#maxWalkDistance').val(),
+            maxWalkDistance: maxDistance,
             //triangleSafetyFactor: $('#').val(),
             //triangleSlopeFactor: $('#').val(),
             //triangleTimeFactor: $('#').val(),
@@ -269,7 +276,24 @@ var OtpRequestFormView = Backbone.View.extend({
 
         this.model.set(data);
 
+    },
+
+    updateModeControls : function() {
+        var mode = $('#mode').val();
+
+        if(mode.indexOf("WALK") != -1 && mode.indexOf("TRANSIT") != -1) this.$el.find(".maxWalkControl").show();
+        else this.$el.find(".maxWalkControl").hide();
+
+        if(mode.indexOf("BICYCLE") != -1 && mode.indexOf("TRANSIT") != -1) this.$el.find(".maxBikeControl").show();
+        else this.$el.find(".maxBikeControl").hide();
+
+        if(mode.indexOf("TRANSIT") != -1) this.$el.find(".optimizeControl").show();
+        else this.$el.find(".optimizeControl").hide();
+
+        if(mode.indexOf("BICYCLE") != -1) this.$el.find(".bikeTriangleControl").show();
+        else this.$el.find(".bikeTriangleControl").hide();
     }
+
 });
 
 module.exports.OtpRequestFormView = OtpRequestFormView;
