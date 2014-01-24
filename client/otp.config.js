ccc4b6366a647f5e6e6d5c4b1846159c865d203d
David Emory <demory@conveyal.com>
2014-01-24 01:03:30 -0500

    restore default config

--- client/otp.config.js
+++ client/otp.config.js
@@ -1,12 +1,12 @@
 
 var OTP_config = {
 	
-	"initLatLng" : [-36.840417, 174.739869],
+	"initLatLng" : [44.9473,-123.0564],
 
 	"osmMapKey": "conveyal.map-jc4m5i21",
 	"aerialMapKey": "conveyal.map-a3mk3jug",
 
-	"otpApi": "http://54.252.109.53:8001/otp-rest-servlet/ws",
+	"otpApi": "http://localhost:8001/otp-rest-servlet/ws",
 	//"simplecoderApi": "http://localhost:9000",
 
 	"reverseGeocode": false
