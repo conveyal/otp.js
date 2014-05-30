a4275ad4f877650d9d8cc6478dd8bcb8e21d940d
Kevin Webb <kpwebb@gmail.com>
2014-05-30 15:47:15 -0400

    update to use select2

--- client/otp.config.js
+++ client/otp.config.js
@@ -6,8 +6,8 @@ var OTP_config = {
 	"osmMapKey": "conveyal.map-jc4m5i21",
 	"aerialMapKey": "conveyal.map-a3mk3jug",
 
-	"otpApi": "http://localhost:8001/otp-rest-servlet/ws",
-	//"simplecoderApi": "http://localhost:9000",
+	"otpApi": "http://maps.cherriots.org:8080/opentripplanner-api-webapp/ws",
+	"esriApi": "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/",
 
 	"reverseGeocode": false
 
