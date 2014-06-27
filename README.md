1f598d9a83ac075eaa9cdbc1bd4627c956279108
David Emory <demory@conveyal.com>
2014-06-27 15:18:57 +0200

    Update README and comment client/index.js

---
+++ README.md
@@ -0,0 +1,18 @@
+
+otp.js is a lightweight library for modular development of modern [OpenTripPlanner](http://opentripplanner.org) front-end interfaces.
+
+The core of the library is a collection of data models and views (`lib/`), written using backbone.js, that encapuslate OTP API requests and responses. Also included is a simple web client (`client/`) that shows how otp.js can be used to create a functional OTP front-end.
+
+otp.js uses [Component](https://github.com/component/component) for dependency and build process management.
+
+To get started with otp.js and the sample web client:
+
+1. Clone the otp.js repo to your local machine using `git clone https://github.com/conveyal/otp.js.git`
+
+2. Install [npm](https://www.npmjs.org/) if needed. In the otp.js project directory, run `npm install` to install Component and set up the development environment (this in configured in package.json).
+
+3. Run `make`, which invokes Component to download any required dependencies and package them together with the local otp.js code (a copy of the packaged code is put in `client/build`).
+
+4. Update the default configuration in `client/otp.config.js` to point to your OTP backend.
+
+5. Deploy the client to the web by copying the `client/` directory to your web server.
--- client/index.js
+++ client/index.js
@@ -7,36 +7,48 @@ OTP.config = OTP_config;
 
 $(document).ready(function() {
 
-
+    // set up the leafet map object
 	var map = L.map('map').setView(OTP.config.initLatLng, (OTP.config.initZoom || 13));
     map.attributionControl.setPrefix('');
-    
-	// add an OpenStreetMap tile layer
+
+	// create OpenStreetMap tile layers for streets and aerial imagery
 	var osmLayer = L.tileLayer('http://{s}.tiles.mapbox.com/v3/' + OTP.config.osmMapKey + '/{z}/{x}/{y}.png', {
         subdomains : ['a','b','c','d'],
 	    attribution: 'Street Map: <a href="http://mapbox.com/about/maps">Terms & Feedback</a>'
 	});
-    
     var aerialLayer = L.tileLayer('http://{s}.tiles.mapbox.com/v3/' + OTP.config.aerialMapKey + '/{z}/{x}/{y}.png', {
         subdomains : ['a','b','c','d'],
         attribution : 'Satellite Map: <a href="http://mapbox.com/about/maps">Terms & Feedback</a>'
     });
 
+    // create a leaflet layer control and add it to the map
     var baseLayers = {
         "Street Map" : osmLayer,
         "Satellite Map" : aerialLayer
     };
     L.control.layers(baseLayers).addTo(map);
+
+    // display the OSM street layer by default
     osmLayer.addTo(map);
 
-    var stopsRequestModel = new OTP.models.OtpStopsInRectangleRequest();//{}, {urlRoot: OTP.config.otpApi + '/transit/stopsInRectangle' });
+    // create the trip topography widget and add it to the map
+    var topoControl = new OTP.topo_views.LeafletTopoGraphControl();
+    topoControl.addTo(map);
+
+    // create a data model for the currently visible stops, and point it
+    // to the corresponding API method
+    var stopsRequestModel = new OTP.models.OtpStopsInRectangleRequest();
     stopsRequestModel.urlRoot = OTP.config.otpApi + '/transit/stopsInRectangle';
 
-
+    // create the stops request view, which monitors the map and updates the
+    // bounds of the visible stops request as the viewable area changes
     var stopsRequestMapView = new OTP.map_views.OtpStopsRequestMapView({
         model: stopsRequestModel,
         map: map
     });
+
+    // create the stops response view, which refreshes the stop markers on the
+    // map whenever the underlying visible stops model changes
     var stopsResponseMapView = new OTP.map_views.OtpStopsResponseMapView({
         map: map
     });
@@ -44,50 +56,52 @@ $(document).ready(function() {
         stopsResponseMapView.newResponse(response);
     });
 
-    var topoControl = new OTP.topo_views.LeafletTopoGraphControl();
-    topoControl.addTo(map);
-
-    var requestModel = new OTP.models.OtpPlanRequest();//{}, {urlRoot: OTP.config.otpApi + '/plan' }); 
+    // create the main OTP trip plan request model and point it to the API
+    var requestModel = new OTP.models.OtpPlanRequest();
     requestModel.urlRoot = OTP.config.otpApi + '/plan';
 
+    // create and render the main request view, which displays the trip
+    // preference form
     var requestView = new OTP.request_views.OtpRequestFormView({
         model: requestModel,
         el: $('#request')
     });
-
     requestView.render();
 
+    // create and render the request map view, which handles the map-specific
+    // trip request elements( e.g. the start and end markers)
     var requestMapView = new OTP.map_views.OtpRequestMapView({
     	model: requestModel,
     	map: map
     });
     requestMapView.render();
 
+    // create the main response view, which refreshes the trip narrative display
+    // and map elements as the underlying OTP response changes
     var responseView = new OTP.views.OtpPlanResponseView({
         narrative: $('#narrative'),
         map: map,
         topo: topoControl.getGraphElement()
     });
 
+    // instruct the response view to listen to relevant request model events
     requestModel.on('success', function(response) {
         responseView.newResponse(response);
     });
-
     requestModel.on('failure', function(response) {
         responseView.newResponse(response);
     });
 
     requestModel.request();
 
+    // make the UI responsive to resizing of the containing window
     var resize = function() {
         var height = $(window).height() - 30;
         $('#map').height(height);
         $('#sidebar').height(height);
-        map.invalidateSize(); 
+        map.invalidateSize();
     };
-
     $(window).resize(resize);
-
     resize.call();
 });
 
