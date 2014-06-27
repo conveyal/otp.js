4b945cb34a7e850a389144456664551e4af96084
Kevin Webb <kpwebb@gmail.com>
2014-06-27 06:35:15 -0400

    Update otp.config.js

--- client/otp.config.js
+++ client/otp.config.js
@@ -3,8 +3,8 @@ var OTP_config = {
 	
 	"initLatLng" : [44.9473,-123.0564],
 
-	"osmMapKey": "conveyal.map-jc4m5i21",
-	"aerialMapKey": "conveyal.map-a3mk3jug",
+	"osmMapKey": "conveyal.ikck6888", // temporary -- do not use in production, provide your own
+	"aerialMapKey": "--", // unset
 
 	"otpApi": "http://localhost:8001/otp-rest-servlet/ws",
 	//"simplecoderApi": "http://localhost:9000",
