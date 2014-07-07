b676a26778c843b111ca85f5cccc6d2877fc7218
Kevin Webb <kpwebb@gmail.com>
2014-07-07 11:21:42 -0400

    fixed handling of failed/incomplete requests

--- client/otp.config.js
+++ client/otp.config.js
@@ -4,7 +4,7 @@ var OTP_config = {
 	"initLatLng" : [44.9473,-123.0564],
 
 	"osmMapKey": "conveyal.ikck6888", // temporary -- do not use in production, provide your own
-	"aerialMapKey": "--", // unset
+	"aerialMapKey": "conveyal.map-a3mk3jug", // unset
 
 	"otpApi": "http://maps.cherriots.org:8080/opentripplanner-api-webapp/ws",
 	"esriApi": "http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/",
