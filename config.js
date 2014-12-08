fb60c1f781c638f530d12ee1bc84d8b3f48fcd88
Kevin Webb <kpwebb@gmail.com>
2014-12-08 07:03:08 -0500

    image + style changes

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ window.OTP_config = {
   osmMapKey: 'conveyal.ikck6888', // temporary -- do not use in production, provide your own
   aerialMapKey: 'conveyal.map-a3mk3jug', // unset
 
-  otpApi: 'http://otp-test.nysdot.conveyal.com/otp/routers/',
-  esriApi: 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
+  otpApi: '/otp/routers/',
+  esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
 
   reverseGeocode: true
 };
