e1185a77dcc6f7fd15e42e96d2cfce70c9586c2f
Gustavo Caparica <gustavo.caparica@gmail.com>
2015-10-02 20:14:49 -0300

    Add support for configuring reverse geocoders
    Also add the configuration variables to client/config.js

--- client/config.js
+++ client/config.js
@@ -4,8 +4,16 @@ window.OTP_config = {
   osmMapKey: 'conveyal.ikck6888', // temporary -- do not use in production, provide your own
   aerialMapKey: 'conveyal.map-a3mk3jug', // unset
 
-  otpApi: 'https://otp.511ny.org/otp/routers/',
-  esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
+  // mapboxAccessToken: 'Change this to your mapbox public token (starts with pk) if you want to use the mapbox geocoder',
 
-  reverseGeocode: true
+  otpApi: 'http://192.168.59.103:8080/otp/routers/',
+
+  // geocoders to use:
+  geocoders: [ 'esri' ],
+  reverseGeocoder: 'esri', // possible choices: esri, nominatim
+  reverseGeocode: true,
+
+  // geocoder api endpoints
+  nominatimApi: 'https://nominatim.openstreetmap.org/',
+  esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
 }
