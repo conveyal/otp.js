b0859b88806c83f066a10a8ca3126848112a8fd2
David Emory <demory@conveyal.com>
2016-02-19 18:18:53 -0500

    Pelias -> Mapzen Search

--- client/config.js
+++ client/config.js
@@ -1,20 +1,21 @@
 window.OTP_config = {
-  initLatLng: [38.880148, -77.105933],
+  initLatLng: [45.52, -122.681944],
 
-  osmMapKey: 'conveyal.ikck6888', // temporary -- do not use in production, provide your own
-  aerialMapKey: 'conveyal.map-a3mk3jug', // unset
+  osmMapKey: 'your-mapbox-osm-key',
+  aerialMapKey: 'your-mapbox-aerial-key',
 
-  // mapboxAccessToken: 'Change this to your mapbox public token (starts with pk) if you want to use the mapbox geocoder',
+  // mapzenApiKey: "your-key", // your Mapzen Search API key
+  // geocoderSearchRadius : 25, // search radius in km for mapzen geocoder from initLatLng (defaults to 50 if not specified)
 
-  otpApi: 'http://192.168.59.103:8080/otp/routers/',
+  otpApi: 'http://localhost:8001/otp/routers/',
 
   // geocoders to use:
-  geocoders: [ 'esri' ], // possible choices: esri, otp, nominatim, pelias, mapbox
-  reverseGeocoder: 'esri', // possible choices: esri, nominatim, pelias, mapbox
+  geocoders: [ 'edsi' ], // possible choices: esri, otp, nominatim, mapzen, mapbox
+  reverseGeocoder: 'esri', // possible choices: esri, nominatim, mapzen, mapbox
   reverseGeocode: true,
 
   // geocoder api endpoints
   nominatimApi: 'https://nominatim.openstreetmap.org/',
   esriApi: 'https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/',
-  peliasApi: 'https://pelias.mapzen.com/',
+  mapzenApi: 'https://search.mapzen.com/v1/'
 }
